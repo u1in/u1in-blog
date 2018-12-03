5350e6a7e9e61fafd65d7714cd1ccdad75de959c
U1in <714896166@qq.com>
2018-12-04 01:35:04 +0800

    add trycatch

--- server.js
+++ server.js
@@ -41,115 +41,145 @@ app.use(views(__dirname + '/views/html', {
 
 //重定向/到index
 router.all('/', async(ctx, next) => {
-    ctx.redirect(encodeURI('/index'));
+    try {
+        ctx.redirect(encodeURI('/index'));
+    } catch (error) {
+        console.log(error);
+        ctx.response.status = 404;
+    }
 })
 
 
 //重定向没有页数的index到第一页
 router.all('/index', async (ctx, next) => {
-    ctx.redirect(encodeURI('/index/1'));
+    try {
+        ctx.redirect(encodeURI('/index/1'));
+    } catch (error) {
+        console.log(error);
+        ctx.response.status = 404;
+    }
 })
 
 //文章列表路由
 router.get('/index/:page', async(ctx, next) => {
-    //每页文章数
-    const pageNumber = 8;
+    try {
+        //每页文章数
+        const pageNumber = 8;
 
-    //当前页数
-    const page = parseInt(ctx.params.page);
+        //当前页数
+        const page = parseInt(ctx.params.page);
 
-    //所有文章列表
-    let list = await markdown();
+        //所有文章列表
+        let list = await markdown();
 
-    //总页数
-    const totalPage = Math.ceil(list.length / pageNumber);
+        //总页数
+        const totalPage = Math.ceil(list.length / pageNumber);
 
 
-    //分页按钮， 以最多6个分页按钮设计
-    let pagination = [];
+        //分页按钮， 以最多6个分页按钮设计
+        let pagination = [];
 
-    //生成分页按钮
-    //懒了 先暴力
-    if(page <= totalPage ) {
+        //生成分页按钮
+        //懒了 先暴力
+        if (page <= totalPage) {
 
-        if (page <= 3 && totalPage - page >= 3) {
-            pagination = [1,2,3,4,5,6];
-        }
-        else if (page <= 3 && totalPage - page <= 3) {
-            for (let i = 1; i <= totalPage; i++) {
-                pagination.push(i);
+            if (page <= 3 && totalPage - page >= 3) {
+                pagination = [1, 2, 3, 4, 5, 6];
             }
-        }
-        else if (page >= 3 && totalPage - page >= 3) {
-            pagination = [page - 2, page - 1, page, page + 1, page + 2 , page + 3]
-        }
-        else if (page >= 3 && totalPage - page <= 3){
-            pagination = [totalPage - 5, totalPage - 4, totalPage - 3, totalPage - 2, totalPage - 1, totalPage];
-        }
+            else if (page <= 3 && totalPage - page <= 3) {
+                for (let i = 1; i <= totalPage; i++) {
+                    pagination.push(i);
+                }
+            }
+            else if (page >= 3 && totalPage - page >= 3) {
+                pagination = [page - 2, page - 1, page, page + 1, page + 2, page + 3]
+            }
+            else if (page >= 3 && totalPage - page <= 3) {
+                pagination = [totalPage - 5, totalPage - 4, totalPage - 3, totalPage - 2, totalPage - 1, totalPage];
+            }
 
-        //填充至模板返回
-        await ctx.render('index', {
-            title: '首页 | 右灵',
-            articles: list.splice(0 + (page - 1) * pageNumber, pageNumber),
-            pagination,
-            currentPage: page,
-            totalPage,
-        })
-    }
-    else {
+            //填充至模板返回
+            await ctx.render('index', {
+                title: '首页 | 右灵',
+                articles: list.splice(0 + (page - 1) * pageNumber, pageNumber),
+                pagination,
+                currentPage: page,
+                totalPage,
+            })
+        }
+        else {
 
-        //页码不合规范重定向
-        ctx.redirect(encodeURI('/index/1'));
+            //页码不合规范重定向
+            ctx.redirect(encodeURI('/index/1'));
+        }
+    } catch (error) {
+        console.log(error);
+        ctx.response.status = 404;
     }
 })
 
 
 //文章路由
 router.get('/article/:name', async (ctx, next) => {
-    //url解码文章名
-    let mdName =  decodeURI(ctx.params.name);
+    try {
+        //url解码文章名
+        let mdName = decodeURI(ctx.params.name);
 
-    //读取文章字符串
-    let html = fs.readFileSync(path.join(__dirname, `./post/${mdName}.md`), 'utf-8');
+        //读取文章字符串
+        let html = fs.readFileSync(path.join(__dirname, `./post/${mdName}.md`), 'utf-8');
 
-    //md转换成html
-    let htmarkedHTMLml = marked(html);
+        //md转换成html
+        let htmarkedHTMLml = marked(html);
 
-    //填充至模板返回
-    await ctx.render('article', {
-        title: `${mdName.split('@')[1]} | 右灵`,
-        text: htmarkedHTMLml,
-    })
+        //填充至模板返回
+        await ctx.render('article', {
+            title: `${mdName.split('@')[1]} | 右灵`,
+            text: htmarkedHTMLml,
+        })
+    } catch (error) {
+        console.log(error);
+        ctx.response.status = 404;
+    }
 })
 
 //关于路由
 router.get('/about', async (ctx, next) => {
-    //读取文章字符串
-    let html = fs.readFileSync(path.join(__dirname, `./about/about.md`), 'utf-8');
+    try {
+        //读取文章字符串
+        let html = fs.readFileSync(path.join(__dirname, `./about/about.md`), 'utf-8');
 
-    //md转换html
-    let htmarkedHTMLml = marked(html);
+        //md转换html
+        let htmarkedHTMLml = marked(html);
 
-    //填充至模板返回
-    await ctx.render('article', {
-        title: '关于 | 右灵',
-        text: htmarkedHTMLml,
-    })
+        //填充至模板返回
+        await ctx.render('article', {
+            title: '关于 | 右灵',
+            text: htmarkedHTMLml,
+        })
+    } catch (error) {
+        console.log(error);
+        ctx.response.status = 404;
+    }
 })
 
 //友链路由
 router.get('/link', async (ctx, next) => {
-    //读取文章字符串
-    let html = fs.readFileSync(path.join(__dirname, `./link/link.md`), 'utf-8');
+    try {
+        //读取文章字符串
+        let html = fs.readFileSync(path.join(__dirname, `./link/link.md`), 'utf-8');
 
-    //md转换html
-    let htmarkedHTMLml = marked(html);
+        //md转换html
+        let htmarkedHTMLml = marked(html);
 
-    //填充值模板返回
-    await ctx.render('article', {
-        title: '友链 | 右灵',
-        text: htmarkedHTMLml,
-    })
+        //填充值模板返回
+        await ctx.render('article', {
+            title: '友链 | 右灵',
+            text: htmarkedHTMLml,
+        })
+    } catch (error) {
+        console.log(error);
+        ctx.response.status = 404;
+    }
 })
 
 //应用路由
